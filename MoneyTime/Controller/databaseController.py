8a40c69858478f0304d55ae788232cac2776b458
Alvin Ababil <neoryumasil@gmail.com>
2026-01-26 13:20:36 +0800

    CHANGE : Database Azure to Supabase

--- MoneyTime/Controller/databaseController.py
+++ MoneyTime/Controller/databaseController.py
@@ -1,29 +1,20 @@
-import pymssql
+import psycopg2
 import os
 
 def db_connect():
     try:
-        # Koneksi menggunakan pymssql (tidak butuh driver ODBC di Vercel)
-        connection = pymssql.connect(
-            server=os.getenv('DB_SERVER'),
-            user=os.getenv('DB_USERNAME'),
-            password=os.getenv('DB_PASSWORD'),
-            database=os.getenv('DB_DATABASE'),
-            port=1433,
-            timeout=30
+        # Koneksi
+        connection = psycopg2.connect(
+            host = os.getenv('DB_SERVER'),
+            user = os.getenv('DB_USERNAME'),
+            password = os.getenv('DB_PASSWORD'),
+            dbname = os.getenv('DB_DATABASE'),
+            port = "5342",
+            connect_timeout=10
         )
+        
         return connection
+    
     except Exception as e:
         print(f"Gagal koneksi ke Azure SQL: {e}")
         return None
-
-# Contoh query untuk mengambil data dari tabel 'user'
-#sql = ''' SELECT * FROM [dbo].[user] '''
-
-# Eksekusi query dan mengambil data
-#cursor = connection.cursor()
-#cursor.execute(sql)
-
-# Menampilkan data yang diambil
-#data = cursor.fetchall()
-#print(data)
