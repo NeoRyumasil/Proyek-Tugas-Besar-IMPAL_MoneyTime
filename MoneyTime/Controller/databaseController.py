6b75259126661eeb40f9a26676430d5311341a15
Alvin Ababil <neoryumasil@gmail.com>
2026-01-31 22:23:50 +0800

    FIX : Database to Vercel Connection

--- MoneyTime/Controller/databaseController.py
+++ MoneyTime/Controller/databaseController.py
@@ -2,20 +2,27 @@ import psycopg2
 import os
 
 def db_connect():
+    import psycopg2
+import os
+
+def db_connect():
     try:
-        # Koneksi
-        connection = psycopg2.connect(
-            host = os.getenv('DB_SERVER'),
-            user = os.getenv('DB_USERNAME'),
-            password = os.getenv('DB_PASSWORD'),
-            dbname = os.getenv('DB_DATABASE'),
-            port = "6543",
-            connect_timeout=10,
-            sslmode = "require"
-        )
+        db_url = os.getenv('DATABASE_URL')
+        
+        if db_url:
+            connection = psycopg2.connect(db_url, sslmode="require")
+        else:
+            connection = psycopg2.connect(
+                host=os.getenv('DB_SERVER'),
+                user=os.getenv('DB_USERNAME'),
+                password=os.getenv('DB_PASSWORD'),
+                dbname=os.getenv('DB_DATABASE'),
+                port=os.getenv('DB_PORT', "5432"),
+                sslmode="require"
+            )
         
         return connection
     
     except Exception as e:
-        print(f"Gagal koneksi ke Azure SQL: {e}")
+        print(f"Gagal koneksi ke Database: {e}")
         return None
