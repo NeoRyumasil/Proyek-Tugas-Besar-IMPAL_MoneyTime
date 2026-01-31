0fa2f2d51172f2c5c621cf0454fc97f6cb671b47
Alvin Ababil <neoryumasil@gmail.com>
2026-01-31 23:02:28 +0800

    FIX : UWU

--- MoneyTime/Controller/databaseController.py
+++ MoneyTime/Controller/databaseController.py
@@ -1,18 +1,26 @@
 import psycopg2
 import os
+from urllib.parse import urlparse, urlunparse
 
 def db_connect():
-    
     try:
-        connection = psycopg2.connect(
-            host=os.getenv('POSTGRES_HOST'),
-            user=os.getenv('POSTGRES_USER'),
-            password=os.getenv('POSTGRES_PASSWORD'),
-            dbname=os.getenv('POSTGRES_DATABASE'),
-            port="5432",
-            sslmode="require"
-        )
-        return connection
+        db_url = os.getenv('POSTGRES_URL_NON_POOLING')
+        
+        if db_url:
+            parsed = urlparse(db_url)
+            clean_url = urlunparse(parsed._replace(query=""))
+            connection = psycopg2.connect(clean_url, sslmode="require")
+            return connection
+            
+        else:
+            return psycopg2.connect(
+                host=os.getenv('POSTGRES_HOST'),
+                user=os.getenv('POSTGRES_USER'),
+                password=os.getenv('POSTGRES_PASSWORD'),
+                dbname=os.getenv('POSTGRES_DATABASE'),
+                port="5432",
+                sslmode="require"
+            )
     
     except Exception as e:
         print(f"Gagal koneksi ke Database: {e}")
