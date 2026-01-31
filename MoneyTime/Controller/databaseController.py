c8ae2f74f412db8382def946d84970eb149d9438
Alvin Ababil <neoryumasil@gmail.com>
2026-01-31 22:40:30 +0800

    FIX : Ini dah terintegrasi harusnya

--- MoneyTime/Controller/databaseController.py
+++ MoneyTime/Controller/databaseController.py
@@ -7,16 +7,16 @@ import os
 
 def db_connect():
     try:
-        db_url = os.getenv('DATABASE_URL')
+        db_url = os.getenv('POSTGRES_URL')
         
         if db_url:
             connection = psycopg2.connect(db_url, sslmode="require")
         else:
             connection = psycopg2.connect(
-                host=os.getenv('DB_SERVER'),
-                user=os.getenv('DB_USERNAME'),
-                password=os.getenv('DB_PASSWORD'),
-                dbname=os.getenv('DB_DATABASE'),
+                host=os.getenv('POSTGRES_HOST'),
+                user=os.getenv('POSTGRES_USER'),
+                password=os.getenv('POSTGRES_PASSWORD'),
+                dbname=os.getenv('POSTGRES_DATABASE'),
                 port=os.getenv('DB_PORT', "5432"),
                 sslmode="require"
             )
