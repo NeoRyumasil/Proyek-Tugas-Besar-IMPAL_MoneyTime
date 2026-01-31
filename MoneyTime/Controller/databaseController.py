8bf5398463a8f8cfc44a8dc76e999a47292a6d3e
Alvin Ababil <neoryumasil@gmail.com>
2026-01-31 22:46:07 +0800

    FIX : Apakah MAsih Error ini ><

--- MoneyTime/Controller/databaseController.py
+++ MoneyTime/Controller/databaseController.py
@@ -5,12 +5,19 @@ def db_connect():
     import psycopg2
 import os
 
+import psycopg2
+import os
+from urllib.parse import urlparse, urlunparse
+
 def db_connect():
     try:
         db_url = os.getenv('POSTGRES_URL')
         
         if db_url:
-            connection = psycopg2.connect(db_url, sslmode="require")
+            parsed = urlparse(db_url)
+            clean_url = urlunparse(parsed._replace(query=""))
+            connection = psycopg2.connect(clean_url, sslmode="require")
+
         else:
             connection = psycopg2.connect(
                 host=os.getenv('POSTGRES_HOST'),
@@ -25,4 +32,3 @@ def db_connect():
     
     except Exception as e:
         print(f"Gagal koneksi ke Database: {e}")
-        return None
