16d45cd69f5517c8c60ecb1936b599f485c5983b
Alvin Ababil <neoryumasil@gmail.com>
2026-07-18 11:28:24 +0700

    Fix ORM Error on Deploy

--- MoneyTime/Database/models.py
+++ MoneyTime/Database/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, Text, Date, Numeric, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
-from orm import base
+from Database.orm import base
 
 # User Class
 class User(base):
